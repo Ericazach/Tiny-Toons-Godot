8b5049c6789251c031d106d0ed7668caea4927f9
Erica Sanchez <ericazach@gmail.com>
2024-09-25 19:15:28 +0200

    make the player change sprites

--- scenes/player.gd
+++ scenes/player.gd
@@ -6,6 +6,10 @@ extends CharacterBody2D
 @export var jump_speed = 100
 @export var speed = 100
 
+@onready var ap = $AnimationPlayer
+@onready var sprite = $Sprite2D
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -22,6 +26,9 @@ func _physics_process(delta):
 	var direction = Input.get_axis("left", "right")
 	velocity.x = speed * direction
 	
+	if direction != 0:
+		sprite.flip_h = (direction == -1)
+	
 	#gravity
 	if not is_on_floor():
 		velocity.y = velocity.y + gravity + delta
@@ -35,3 +42,17 @@ func _physics_process(delta):
 		velocity.y = velocity.y - jump_speed
 		
 	move_and_slide()
+	
+	update_animation(direction)
+	
+func update_animation(direction):
+	if is_on_floor():
+		if direction == 0:
+			ap.play("idle")
+		else:
+			ap.play("run")
+	else:
+		if velocity.y < 0:
+			ap.play("jump")
+		elif velocity.y > 0:
+			ap.play("fall")
